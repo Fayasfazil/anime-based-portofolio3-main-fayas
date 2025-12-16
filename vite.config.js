11c381d0268d1e29e02dabeeb9519d3114a02a50
Fayasfazil <fayashamed2004@gmail.com>
2025-12-17 02:03:30 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// ... nothing else in this file ...
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // points to: c:\Users\Asus\Desktop\FAYAS PROJEcts\anime baesd portofolio3-main\portofolio3-main\portofolio3
+  root: 'portofolio3-main/portofolio3',
+  server: { open: true }
+});
+// ... nothing else in this file ...
